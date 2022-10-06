1b0be8ddb919c9c266a5dc6c5d3d96e27bbbdc81
GabrielHDZ <gabrielhdz0102@gmail.com>
2022-10-05 22:42:20 -0500

    update css stylesheet

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-document.querySelector('#presentacion').style.display='block';
+//document.querySelectorAll("section").style.display = 'block';
 
-const SecActive=(rep)=>{
-    //obtenemos un NodeList del DOM
-    const NodeList=document.querySelectorAll('.seccion');
+const SecActive = (rep) => {
+    /* //obtenemos un NodeList del DOM
+    const NodeList = document.querySelectorAll('.seccion');
     // Change the text of multiple elements with a loop
     NodeList.forEach(e => {
-    e.style.display='none';
+        e.style.display = 'none';
     });
-    let seccion=rep.hash.slice(1);
-    document.getElementById(seccion).style.display='block';
+    let seccion = rep.hash.slice(1);
+    document.getElementById(seccion).style.display = 'block'; */
 }
