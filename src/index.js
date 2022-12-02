bc0222b66b47dab54a9ed76467ad3bc4ef630923
GabrielHDZ <gabrielhdz0102@gmail.com>
2022-12-01 21:29:27 -0600

    update ideas for my portfolio

--- src/index.js
+++ src/index.js
@@ -5,6 +5,73 @@ window.onscroll = () => {
 
 };
 
+window.addEventListener('mouseup', (e) => {
+    console.log(e.button);
+})
+
+const normalizePozition = (mouseX, mouseY) => {
+    // ? compute what is the mouse position relative to the container element (scope)
+    const {
+        left: scopeOffsetX,
+        top: scopeOffsetY,
+    } = scope.getBoundingClientRect();
+
+    const scopeX = mouseX - scopeOffsetX;
+    const scopeY = mouseY - scopeOffsetY;
+
+    // ? check if the element will go out of bounds
+    const outOfBoundsOnX =
+        scopeX + contextMenu.clientWidth > scope.clientWidth;
+
+    const outOfBoundsOnY =
+        scopeY + contextMenu.clientHeight > scope.clientHeight;
+
+    let normalizedX = mouseX;
+    let normalizedY = mouseY;
+
+    // ? normalzie on X
+    if (outOfBoundsOnX) {
+        normalizedX =
+            scopeOffsetX + scope.clientWidth - contextMenu.clientWidth;
+    }
+    if (outOfBoundsOnY) {
+        normalizedY =
+            scopeOffsetY + scope.clientHeight - contextMenu.clientHeight;
+    }
+    return { normalizedX, normalizedY };
+
+
+}
+const contextMenu = document.getElementById('context-menu');
+const scope = document.querySelector("body");
+scope.addEventListener("contextmenu", (e) => {
+    e.preventDefault();
+    const { clientX: mouseX, clientY: mouseY } = e;
+    contextMenu.style.top = `${mouseY}px`;
+    contextMenu.style.left = `${mouseX}px`;
+
+
+    /* const { offsetX: mouseX, offsetY: mouseY } = e;
+    const { normalizedX, normalizedY } = normalizePozition(mouseX, mouseY);
+    contextMenu.style.top = `${normalizedY}px`;
+    contextMenu.style.left = `${normalizedX}px`;
+ */
+    contextMenu.classList.remove('visible');
+    setTimeout(() => {
+        contextMenu.classList.add('visible');
+
+    })
+});
+scope.addEventListener('click', (e) => {
+    if (e.target.offsetParent != contextMenu) {
+        contextMenu.classList.remove('visible');
+
+    }
+})
+contextMenu.addEventListener('click', (e) => {
+    contextMenu.classList.remove('visible');
+})
+
 
 
 const SecActive = (rep) => {
