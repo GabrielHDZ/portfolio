19ae44cb28e0431054b87621257e46455c44f38a
GabrielHDZ <gabrielhdz0102@gmail.com>
2023-11-14 20:16:51 -0600

    three

--- main.js
+++ main.js
@@ -1,13 +1,14 @@
 import * as THREE from 'three';
-
+//escenario,camara,iluminacion
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-
-const renderer = new THREE.WebGLRenderer();
+//motor renderizador
+const renderer = new THREE.WebGLRenderer({ alpha: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-const geometry = new THREE.PlaneGeometry(10, 10);
+//geometria,textura,animaciones
+const geometry = new THREE.PlaneGeometry(2, 2);
 const material = new THREE.MeshBasicMaterial({ color: 0x148f77 });
 const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
